8c0e731974f8d2dc95374f9802b89799f4755d59
Silas Felipe Garcia <silas.garcia@mfconsulting.com.br>
2018-02-14 23:57:44 -0200

    Criando temporizador por sessão
    criando temporizador aumentando mais 10 minutos a cada requisição que o usuário faz quando está logado, aumentando o tempo automáticamente para que não deslogue enquanto utiliza.

--- src/main/java/br/com/alura/gerenciador/web/FiltroDeAuditoria.java
+++ src/main/java/br/com/alura/gerenciador/web/FiltroDeAuditoria.java
@@ -11,6 +11,7 @@ import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @WebFilter(urlPatterns="/*")
 public class FiltroDeAuditoria  implements Filter {
@@ -24,8 +25,19 @@ public class FiltroDeAuditoria  implements Filter {
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
 		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse resp = (HttpServletResponse) response;
+		
+		Cookie cookie = new Cookies(req.getCookies()).buscaUsuarioLogado();
+		String usuario = "<deslogado>";
+		
+		if(cookie != null) {		
+			usuario = cookie.getValue();
+			cookie.setMaxAge(10*60);
+			resp.addCookie(cookie);
+		}
+		
 		String uri = req.getRequestURI();
-		String usuario = getUsuario(req);
+		
 		System.out.println("Usuario acessando a URI " + usuario + uri);
 		chain.doFilter(request, response);
 	}
