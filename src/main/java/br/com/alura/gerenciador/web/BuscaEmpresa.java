be7208246e4e7f8ac20f30c2538291fcd5acd464
Silas Felipe Garcia <silas.garcia@mfconsulting.com.br>
2018-02-21 01:00:03 -0300

    Criando Servlets e mostrando no console
    Criação de servlets para mostrar o quando ela é iniciada, quantas vezes é instanciada e quando é destruida, também utilizando váriavel membro (váriavel global) e mostrando erros das quais não se utiliza váriavel membro para um método.

--- src/main/java/br/com/alura/gerenciador/web/BuscaEmpresa.java
+++ src/main/java/br/com/alura/gerenciador/web/BuscaEmpresa.java
@@ -15,19 +15,46 @@ import br.com.alura.gerenciador.dao.EmpresaDAO;
 
 @WebServlet(urlPatterns="/busca")
 public class BuscaEmpresa extends HttpServlet {
-
 	
+	public BuscaEmpresa() {
+		System.out.println("Contruindo uma servlet do tipo BuscaEmpresa" + this);
+	}
+	
+	@Override
+	public void init() throws ServletException {
+		super.init();
+		System.out.println("Inicializando a servlet" + this);
+	}
+	
+	@Override
+	public void destroy() {
+		super.destroy();
+		System.out.println("Destruindo a servlet " + this);
+	}
+	
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
 		PrintWriter writer = resp.getWriter();
-		String filtro = req.getParameter("empresa");
-		Collection<Empresa> empresas = new EmpresaDAO().buscaPorSimilaridade(filtro);
+		
+		
 		
 		writer.println("<html>");
 		writer.println("<body>");
 		writer.println("Resultado da busca: <br/>");
 		writer.println("<ul>");
+		String filtro = req.getParameter("filtro");
+		
+		/* faz com que demore este tempo para continuar o restante do código
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		*/
+		Collection<Empresa> empresas = new EmpresaDAO().buscaPorSimilaridade(filtro);
 		for(Empresa empresa : empresas) {
 			
 			writer.println("<li>" + empresa.getId() + ":" + empresa.getNome() +  "</li>");
